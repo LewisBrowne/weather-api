89dcfc76a15673486cbe8e38915637057e1c6b04
Lewis Browne <lewis@broadlanddigital.co.uk>
2023-04-10 18:21:26 +0100

    Added basic user registration and authentication

---
+++ website_code/api/app/Http/Controllers/UserController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Validator;
+
+
+use App\Models\User;
+
+class UserController extends Controller
+{
+
+    public function login(Request $request){
+        $validator = \Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required', 
+        ]);
+
+        if (!$validator->fails()) {
+            $user = User::where('email', $request->email)->first();
+
+            if (! $user || ! Hash::check($request->password, $user->password)) {
+                return response()->json(['status' => "error", 'message' => 'The provided credentials are incorrect.']);
+            } else {
+                $token = $user->createToken($user->guid)->plainTextToken;
+                return response()->json(['status' => "OK", 'access_token' => $token]);
+            }   
+        } else {
+            return response()->json(['status' => "error", 'message' => $validator->errors()]);
+        }
+    }
+
+
+    public function register(Request $request)
+    {
+        $validator = \Validator::make($request->all(), [
+            'first_name' => 'required|max:60',
+            'last_name' => 'required|max:60',
+            'email' => 'required|unique:users',
+            'password' => 'required',
+           
+        ]);
+    
+        if (!$validator->fails()) {
+            try{
+                $user = User::create([
+                    'first_name' => request('first_name'),
+                    'last_name' => request('last_name'),
+                    'email' => request('email'),
+                    'password' => bcrypt(request('password')),
+                    'guid' => Str::uuid()->toString()
+                ]);
+
+                $token = $user->createToken(Str::uuid()->toString())->plainTextToken;
+            
+                return response()->json(['status' => "OK", 'message' => 'User has been successfully registered.', 'access_token' => $token]);
+            } catch (Exception $e){
+                return response()->json(['status' => "error", 'message' => $e]);
+            }
+        } else {
+            return response()->json(['status' => "error", 'message' => $validator->errors()]);
+        }
+
+    }
+
+
+    // public function test(Request $request)
+    // {
+    //     $user = $request->user();
+    //     return response()->json($user);
+    // }
+
+}
--- website_code/api/app/Models/User.php
+++ website_code/api/app/Models/User.php
@@ -18,9 +18,12 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
+        'guid',
+        'send_daily_forecast'
     ];
 
     /**
