6af375f9bf1d5d3bda91e7cadc1f81196171797a
Lewis Browne <lewis@broadlanddigital.co.uk>
2023-04-11 20:19:17 +0100

    Added basic City model

---
+++ website_code/api/app/Models/City.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class City extends Model
+{
+    use HasFactory;
+
+    protected $table = 'city';
+
+
+    protected $fillable = [
+        'guid',
+        'name',
+        'country'        
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [];
+}
