50a827078b1979705aa89d2dcbe7100429e53270
Lewis Browne <lewis@broadlanddigital.co.uk>
2023-04-17 20:34:40 +0100

    Added dailyforecast email command to schedule

--- website_code/api/app/Console/Kernel.php
+++ website_code/api/app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('city:import')->weekly();
+        $schedule->command('emails:dailyforecast:send')->daily();
     }
 
     /**
