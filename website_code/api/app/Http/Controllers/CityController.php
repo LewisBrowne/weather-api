8a027f96f0d9cf10eb7a83b91092bcba31da00ff
Lewis Browne <lewis@broadlanddigital.co.uk>
2023-04-17 19:37:18 +0100

    Added table fallback when cache empty for city search endpoint

--- website_code/api/app/Http/Controllers/CityController.php
+++ website_code/api/app/Http/Controllers/CityController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 
+use App\Models\City;
+
+
 class CityController extends Controller
 {
     public function list(){
@@ -20,6 +23,18 @@ class CityController extends Controller
         $results = $results->filter(function($city) use ($query) {
             return stripos($city->name,$query) !== false;
         });
+
+        //Fallback to non cached city table
+        if(count($results) == 0){
+            $results = City::all();
+
+            $results =  collect($results);
+            //$results = $results->where('name', 'like', $query); 
+            //$results = $results->search($query);
+            $results = $results->filter(function($city) use ($query) {
+                return stripos($city->name,$query) !== false;
+            });
+        }
 
         if(count($results) == 0){
             return response()->json(['status' => "OK", 'message' => 'Searching for: '.$query, 'results' => null], 404);
